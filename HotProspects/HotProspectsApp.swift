0460d29a3b407bd253ec17f19f11dc1176f12813
Renato Oliveira Fraga <fraga@Renatos-iMac.local>
2021-11-02 20:55:07 -0500

    first commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Renato Oliveira Fraga on 11/2/21.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
