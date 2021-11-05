d19dbd2932a09fa148478373c8e2a68b5c41bfe3
Renato Oliveira Fraga <fraga@Renatos-iMac.local>
2021-11-04 21:43:07 -0500

    day 83 completed

---
+++ HotProspects/Prospect.swift
@@ -0,0 +1,28 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Renato Oliveira Fraga on 11/3/21.
+//
+
+import SwiftUI
+
+class Prospect: Identifiable, Codable {
+    var id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    fileprivate(set) var isContacted = false
+}
+
+class Prospects: ObservableObject {
+    @Published var people: [Prospect]
+    
+    init() {
+        self.people = []
+    }
+    
+    func toggle(_ prospect: Prospect) {
+        objectWillChange.send()
+        prospect.isContacted.toggle()
+    }
+}
